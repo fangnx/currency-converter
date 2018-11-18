3d22bb19fcb6f8c93f3cd7e5f91597014cce1794
fnx <fangnaxin@gmail.com>
2018-11-18 01:30:04 -0500

    Add pyqt gui

--- get_rates.py
+++ get_rates.py
@@ -6,27 +6,60 @@ RATE_TYPES = ["Buying Rate", "Cash Buying Rate", "Selling Rate", "Cash Selling R
 
 
 def main():
-    # currency_type = str(sys.argv[1])
 
-    currency_type = input("Enter the currency type: ")
+    # currency_type = input("Enter the currency type: ")
+    currency_type = "CAD"
 
-    url = 'http://www.boc.cn/sourcedb/whpj/enindex.html'
-    html = urlopen(url).read().decode('utf8')
+    # url = "http://www.boc.cn/sourcedb/whpj/enindex.html"
+    # html = urlopen(url).read().decode('utf8')
+    #
+    # soup = BeautifulSoup(html, features="lxml")
+    # all_tables = soup.find("table", width="600")
+    # all_rows = all_tables.findChildren("tr")
+    #
+    # # Store the currency rates in a dictionary
+    # currency_dict = {row.findChildren("td")[0].string : row.findChildren("td") for row in all_rows[2:]}
 
-    soup = BeautifulSoup(html, features='lxml')
-    all_tables = soup.find('table', width='600')
-    all_rows = all_tables.findChildren('tr')
-
-    # Store the currency rates in a dictionary
-    currency_dict = {row.findChildren('td')[0].string : row.findChildren('td') for row in all_rows[2:]}
+    currency_dict = get_all_rates()
 
     print_all_rates(currency_dict, currency_type)
 
 
+def get_all_rates():
+
+    url = "http://www.boc.cn/sourcedb/whpj/enindex.html"
+    html = urlopen(url).read().decode('utf8')
+
+    soup = BeautifulSoup(html, features="lxml")
+    all_tables = soup.find("table", width="600")
+    all_rows = all_tables.findChildren("tr")
+
+    # Store the currency rates in a dictionary
+    currency_dict = {row.findChildren("td")[0].string: row.findChildren("td") for row in all_rows[2:]}
+
+    return currency_dict
+
+
+def to_string(currency_dict, currency_type):
+
+    output = ""
+
+    currency_row = currency_dict.get(currency_type)
+    for i, type_name in enumerate(RATE_TYPES):
+        output += (type_name + ": " + currency_row[i + 1].string)
+
+    return output
+
+
+
+
+
 def print_all_rates(dict, type):
+
     currency_row = dict.get(type)
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print("The exchange rate from RMB to " + type + ":\n")
+    print("The exchange rate from " + type + " to RMB:\n")
+
     for i, type_name in enumerate(RATE_TYPES):
         print(type_name + ": " + currency_row[i+1].string)
     print("\nPublish Time: " + currency_row[6].string)
---
+++ gui.py
@@ -0,0 +1,82 @@
+from PyQt5.QtWidgets import (QWidget, QApplication, QLabel,
+                             QPushButton, QLineEdit, QGridLayout)
+
+from PyQt5.QtGui import QFont, QIntValidator, QDoubleValidator, QIcon
+from PyQt5.QtCore import Qt, pyqtSlot
+
+import sys
+import get_rates
+
+class MyWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.init_UI()
+        self.setStyleSheet("font-size: 14pt; font-family: Courier")
+
+
+    def init_UI(self):
+
+        self.resize(400, 300)
+        self.move(200, 150)
+        self.setWindowTitle("Simple Currency Rates 💰")
+
+        label_og_amount = QLabel("Amount of RMB")
+        curr_type_label = QLabel("Type of Target Currency")
+        target_amount_label = QLabel("Amount of Target Currency")
+
+        line_og_amount = QLineEdit()
+        line_og_amount.setValidator(QDoubleValidator())
+        line_og_amount.setAlignment(Qt.AlignRight)
+
+        curr_type_edit = QLineEdit()
+        target_amount_edit = QLineEdit()
+
+        btn_enter = QPushButton("Enter", self)
+        btn_enter.clicked.connect(self.on_click_enter)
+
+        grid = QGridLayout()
+        grid.setSpacing(5)
+
+        grid.addWidget(label_og_amount, 0, 0)
+        grid.addWidget(line_og_amount, 0, 1)
+
+        grid.addWidget(curr_type_label, 1, 0)
+        grid.addWidget(curr_type_edit, 1, 1)
+
+        grid.addWidget(target_amount_label, 2, 0)
+        grid.addWidget(target_amount_edit, 2, 1)
+
+        grid.addWidget(btn_enter, 3, 1)
+
+        self.setLayout(grid)
+
+        self.show()
+
+
+    @pyqtSlot()
+    def on_click_enter(self):
+        print("a")
+
+
+    def key_press_handler(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
+
+
+
+
+
+
+def show_mywindow():
+    app = QApplication(sys.argv)
+    w = MyWindow()
+
+    # QLabel(w).setText("<p style='color: blue; margin-left: 20px; margin-right: 20px'><b>hello world</b></p>")
+    QLabel(w).setText("<p style='color: blue; margin-left: 20px; margin-right: 20px>" + get_rates.to_string(get_rates.get_all_rates(), "CAD") + "</p>")
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    show_mywindow()
